d2f2951e1ec2462494872d9523936ed6ac292f6c
Josh Gibson <joshdgibson21@outlook.com>
2022-12-26 14:39:06 +0000

    Added a line to the screen.

--- Source/main.cpp
+++ Source/main.cpp
@@ -9,7 +9,8 @@ int main()
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* Window = SDL_CreateWindow("A SDL3 window", 100, 100, 800, 600, 0);
 	SDL_ShowWindow(Window);
-	
+	SDL_Renderer* Renderer = SDL_CreateRenderer(Window, nullptr, 0);
+	SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
 	bool CloseRequested = false;
 	while (!CloseRequested)
 	{
@@ -22,9 +23,39 @@ int main()
 				CloseRequested = true;
 			}
 		}
+		drawState(Renderer);
+		SDL_RenderPresent(Renderer);
 	}
 	SDL_HideWindow(Window);
 	SDL_DestroyWindow(Window);
 	SDL_Quit();
 	return 0;
+}
+
+void drawState(SDL_Renderer* renderer)
+{
+	int width = 0, height = 0;
+	SDL_GetRendererOutputSize(renderer, &width, &height);
+	SDL_Point LineVerticies[8];
+	LineVerticies[0].x = width * 10 / 100;
+	LineVerticies[1].x = width * 20 / 100;
+	LineVerticies[2].x = width * 30 / 100;
+	LineVerticies[3].x = width * 40 / 100;
+	LineVerticies[4].x = width * 50 / 100;
+	LineVerticies[5].x = width * 60 / 100;
+	LineVerticies[6].x = width * 70 / 100;
+	LineVerticies[7].x = width * 80 / 100;
+	LineVerticies[0].y = height * 10 / 100;
+	LineVerticies[1].y = height * 20 / 100;
+	LineVerticies[2].y = height * 30 / 100;
+	LineVerticies[3].y = height * 40 / 100;
+	LineVerticies[4].y = height * 50 / 100;
+	LineVerticies[5].y = height * 60 / 100;
+	LineVerticies[6].y = height * 70 / 100;
+	LineVerticies[7].y = height * 80 / 100;
+
+	if (SDL_RenderDrawLines(renderer, LineVerticies, 8) != 0)
+	{
+		SDL_Log("Error message: %s \n", SDL_GetError());
+	}
 }
--- Source/main.h
+++ Source/main.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include <SDL3/SDL_render.h>
+
 int main();
+
+void drawState(SDL_Renderer* renderer);
